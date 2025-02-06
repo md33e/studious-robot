ad11ba64be7e43120e3473c16fff48c06e836dcc
md33e <129637500+md33e@users.noreply.github.com>
2025-02-06 08:31:41 -0500

    Update home.js

--- home.js
+++ home.js
@@ -1,4 +1,4 @@
 // When the button is clicked, navigate to the Wanted Poster page
 document.getElementById("go-to-wanted").addEventListener("click", function () {
-    window.location.href = "../Wanted.html"; // Redirects to the wanted poster page
+    window.location.href = "..Wanted.html"; // Redirects to the wanted poster page
 });
