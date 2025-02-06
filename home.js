dccf37bf2d9741a04fff7a94a590e4d9d8bfc02e
md33e <129637500+md33e@users.noreply.github.com>
2025-02-06 08:22:26 -0500

    Create home.js

---
+++ home.js
@@ -0,0 +1,4 @@
+// When the button is clicked, navigate to the Wanted Poster page
+document.getElementById("go-to-wanted").addEventListener("click", function () {
+    window.location.href = "../HTML/Wanted.html"; // Redirects to the wanted poster page
+});
