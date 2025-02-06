a499373fdab60149d1edff0f63bff21800448d59
md33e <129637500+md33e@users.noreply.github.com>
2025-02-06 08:20:53 -0500

    Create Wanted.js

---
+++ Wanted.js
@@ -0,0 +1,4 @@
+// Add functionality to the "Claim Reward" button
+document.getElementById("alert-button").addEventListener("click", function () {
+    alert("You’ve claimed the $5000 bounty!");
+});
